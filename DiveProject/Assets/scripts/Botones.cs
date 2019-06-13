c8a07ae9fbcc4cf626859ca1a92b809fa4370ae4
Gerard <geri271098@gmail.com>
2019-06-13 19:47:16 +0200

    Bases

---
+++ DiveProject/Assets/scripts/Botones.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Botones : MonoBehaviour
+{
+    public string playScene;
+
+    public string settingsScene;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(playScene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene(settingsScene);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ DiveProject/Assets/scripts/MovePlayer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    #region Variables
+    [Header("Movement Settings:")]
+    public float speed;
+
+    [Header("Physics:")]
+    public float fallMultiplier = 2.5f;
+
+    //References
+    [Header("References:")]
+    private Rigidbody rb;
+
+    //Utilities
+    float horizontalInput, verticalInput;
+    Vector3 movementDirection, velocity;
+    bool playerMoving;
+    #endregion
+
+    #region Core
+    // Use this for initialization
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    #endregion
+
+    #region Movement
+    private void Move()
+    {
+        //Get Inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Generate direction vector
+        movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+        //Is the player moving?
+        velocity = new Vector3(horizontalInput, 0, verticalInput);
+
+        if (velocity != Vector3.zero)
+            playerMoving = true;
+        else
+            playerMoving = false;
+
+        //Modify move vector
+        if (verticalInput > 0)
+            movementDirection = transform.forward;
+        else if (verticalInput < 0)
+            movementDirection = -transform.forward;
+
+        if (horizontalInput > 0)
+            movementDirection = transform.right;
+        else if (horizontalInput < 0)
+            movementDirection = -transform.right;
+
+        //Add force in direction with speed
+        rb.AddForce(movementDirection * speed * Time.deltaTime, ForceMode.Force);
+    }
+    #endregion
+
+    #region Utilities
+    private void FixedUpdate()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+    #endregion
+
+}
