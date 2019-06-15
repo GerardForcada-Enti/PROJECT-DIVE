0b87150a3551d5bd51e59bb1c8147527fbf27cb8
Gerard <geri271098@gmail.com>
2019-06-15 12:34:21 +0200

    End lvl+assets config

---
+++ DiveProject/Assets/scripts/ColisionManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ColisionManager : MonoBehaviour
+{
+    public string winScene;
+    public string loseScene;
+    public GameObject mCloud;
+    public GameObject mEnd;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == mEnd)
+        {
+            SceneManager.LoadScene(winScene);
+        }
+        if (collision.gameObject == mCloud)
+        {
+            SceneManager.LoadScene(loseScene);
+        }
+    }
+
+}
